a002abe1cafc444a2d4eb053f8fb7ae792a6877a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 06:26:36 +0800

    siguro mas better ito na way para makuha ko ung ibang nth characters

--- removing_first_n_characters_from_the_strings.py
+++ removing_first_n_characters_from_the_strings.py
@@ -1,8 +1,9 @@
-def removing_the_nth_characters():
-    name = "Sheena Mae"
+def removing_the_nth_characters(name):
     substring1 = name[0:6]
     return substring1
 
-result = removing_the_nth_characters()
+result = removing_the_nth_characters("Sheena Mae")
 print(result)
+
+
 
