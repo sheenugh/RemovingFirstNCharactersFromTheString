5a3cf3a6cdce13c4bcc0cf85ff94460b578aab15
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 06:35:06 +0800

    arrangine and getting another characters from my name

--- removing_first_n_characters_from_the_strings.py
+++ removing_first_n_characters_from_the_strings.py
@@ -1,9 +1,29 @@
-def removing_the_nth_characters(name):
+
+
+
+#---- FUNCTIONS -----
+# - def function of the substring or getting the nth character/s
+def getting_nth_characters(name):
     substring1 = name[0:6]
     return substring1
 
-result = removing_the_nth_characters("Sheena Mae")
-print(result)
+def another_getting_nth_characters(name):
+    substring1 = name[7:]
+    return substring1
+
+
+# PSEUDO CODE
+# - Actual Code
+# - Printing the result from the substring
+result1 = getting_nth_characters("Sheena Mae")
+print(result1)
+
+print("------------------------")
+
+result2= another_getting_nth_characters("Sheena Mae")
+print(result2)
+
+
 
 
 
