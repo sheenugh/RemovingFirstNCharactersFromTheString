ebac3683fcb824bf5cd1df2ce1da30dd07a40f3a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-25 14:09:06 +0800

    changes lang sa paggamit ng words and such characters sa comments

--- removing_first_n_characters_from_the_strings.py
+++ removing_first_n_characters_from_the_strings.py
@@ -1,6 +1,5 @@
 
 
-
 #---- FUNCTIONS -----
 # - def function of the substring or getting the nth character/s
 def getting_nth_characters(name):
@@ -12,13 +11,17 @@ def another_getting_nth_characters(name):
     return substring2
 
 
-# PSEUDO CODE
-# - Actual Code
+#>>>>>>>>>> PSEUDO CODE <<<<<<<<<<
+# ------ ACTUAL CODES -----
 # - Printing the result from the substring 1 and 2
 result1 = getting_nth_characters("Sheena Mae")
 print(result1)
 
+print("\n")
 print("------------------------")
+print("AND/OR")
+print("------------------------")
+print("\n")
 
 result2= another_getting_nth_characters("Sheena Mae")
 print(result2)
